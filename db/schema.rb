251af81adf6e2abb1daa52c35cd19ef0350fcc19
mike vallano <mikevallano@gmail.com>
2024-02-29 10:11:58 -0500

    Add devise gem and setup

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-# rubocop:disable all
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_204436) do
+
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_210601) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_204436) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
-# rubocop:enable all
