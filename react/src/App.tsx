f7b15ce5e14615721c9e946ba027c7df753f13b3
mike vallano <mikevallano@gmail.com>
2024-03-04 11:38:14 -0500

    5: Add component to show content or login form

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,16 +1,18 @@
 import './App.css'
-import LoginForm from './components/LoginForm'
+import LoggedInCheck from './components/LoggedInCheck'
 import Test from './components/Test'
 import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
-  const { isLoggedIn } = useAuthContext()
+  const { isLoggedIn, handleLogOut } = useAuthContext()
   console.log(`isLoggedIn:`, isLoggedIn)
   return (
     <>
+      {isLoggedIn && <button onClick={handleLogOut}>Log out</button>}
       <h2>Goal Tracker</h2>
-      <LoginForm />
-      <Test />
+      <LoggedInCheck>
+        <Test />
+      </LoggedInCheck>
     </>
   )
 }
