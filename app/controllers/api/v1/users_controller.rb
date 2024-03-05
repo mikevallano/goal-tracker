2ffce8fdba579b4ce3b3c7541e29a7b5f24e6687
mike vallano <mikevallano@gmail.com>
2024-03-05 10:47:12 -0500

    2: Add users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < Api::BaseController
+  skip_before_action :authenticate_token!, on: :create
+
+  def create
+    result = CreateUser.call!(email: user_params[:email], password: user_params[:password])
+    if result.success?
+      render json: result.user, status: :created
+    else
+      render json: { error: result.error_message }, status: :precondition_failed
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+end
---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Users', type: :request do
+  describe 'POST /create' do
+    context 'with valid params' do
+      it 'returns http success' do
+        user = build(:user)
+        expect do
+          post('/api/v1/users/create', params: { email: user.email, password: user.password })
+        end.to change { User.count }.by(1)
+        expect(response).to have_http_status(:success)
+        expect(JSON.parse(response.body)['email']).to eq(user.email)
+      end
+    end
+
+    context 'when failure' do
+      it 'returns failure code' do
+        user = build(:user, password: '')
+        post '/api/v1/users/create', params: { email: user.email, password: user.password }
+        expect(response).to have_http_status(:precondition_failed)
+        expect(JSON.parse(response.body)['error']).to include('Password')
+      end
+    end
+  end
+end
