3a8a3cff5a2b755e5030a5d7811ec24e7b8a62ff
mike vallano <mikevallano@gmail.com>
2024-03-13 10:46:48 -0400

    14: Add react router, header, and rearrange components a bit

--- react/src/components/LoggedInCheck.tsx
+++ react/src/components/LoggedInCheck.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react'
 import { useAuthContext } from '../hooks/useAuthContext'
-import LoginOrSignUp from './LoginOrSignUp'
+import Home from './Home'
 
 type LoggedInCheckProps = { children: ReactNode }
 
 const LoggedInCheck: React.FC<LoggedInCheckProps> = ({ children }) => {
   const { isLoggedIn } = useAuthContext()
-  return <>{isLoggedIn ? children : <LoginOrSignUp />}</>
+  return <>{isLoggedIn ? children : <Home />}</>
 }
 export default LoggedInCheck
