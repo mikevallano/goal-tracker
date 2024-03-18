429ae45ca9db368f26f2c3d8a04dbf1bd6e3aecb
mike vallano <mikevallano@gmail.com>
2024-03-18 11:40:55 -0400

    16: Fix serializer to only do serializer logic

--- app/serializers/tracked_goal_serializer.rb
+++ app/serializers/tracked_goal_serializer.rb
@@ -4,6 +4,25 @@ class TrackedGoalSerializer < BaseSerializer
   end
 
   def call!
-    @tracked_goal.attributes.merge('goal' => @tracked_goal.goal.name)
+    serialized_result
   end
+
+  private
+
+  # rubocop:disable Metrics/MethodLength
+  def serialized_result
+    {
+      id: @tracked_goal.id,
+      timeframe_type: @tracked_goal.timeframe_type,
+      progress_rating: @tracked_goal.progress_rating,
+      goal_id: @tracked_goal.goal_id,
+      start_date: @tracked_goal.start_date,
+      end_date: @tracked_goal.end_date,
+      created_at: @tracked_goal.created_at,
+      updated_at: @tracked_goal.updated_at,
+      notes: @tracked_goal.notes,
+      goal_name: @tracked_goal.goal.name
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
 end
