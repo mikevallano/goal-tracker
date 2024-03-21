0932d134d439f82f1598da10293f47da1aa7a10d
mike vallano <mikevallano@gmail.com>
2024-03-21 16:13:56 -0400

    34: Add ability to create categories

--- react/src/components/Categories.tsx
+++ react/src/components/Categories.tsx
@@ -1,13 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAxios } from '../hooks/useAxios'
 import useFetchCategories from '../hooks/useFetchCategories'
 import useGoalManagementContext from '../hooks/useGoalManagementContext'
 import Category from './Category'
+import CategoryForm from './CategoryForm'
 
 const Categories = () => {
   const { loading, error } = useAxios()
   const { categories } = useGoalManagementContext()
   const { fetchCategories } = useFetchCategories()
+  const [isAdding, setIsAdding] = useState(false)
 
   useEffect(() => {
     fetchCategories()
@@ -19,6 +21,12 @@ const Categories = () => {
       {error && <p>{error}</p>}
       {loading && <p>Loading...</p>}
       {!categories && <button onClick={fetchCategories}>Get categories</button>}
+      {!isAdding && (
+        <button className='btn btn-sm' onClick={() => setIsAdding(true)}>
+          Add new category
+        </button>
+      )}
+      {isAdding && <CategoryForm setIsAdding={setIsAdding} />}
       {categories &&
         categories.map((category) => {
           return <Category key={category.id} name={category.name} />
---
+++ react/src/components/CategoryForm.tsx
@@ -0,0 +1,60 @@
+import useGoalManagementContext from '../hooks/useGoalManagementContext'
+import { useAxios } from '../hooks/useAxios'
+import { Dispatch } from 'react'
+
+const CATEGORIES_URL = 'http://localhost:3000/api/v1/categories'
+
+type CategoryFormProps = {
+  setIsAdding: Dispatch<boolean>
+}
+
+const CategoryForm = ({ setIsAdding }: CategoryFormProps) => {
+  const { setCategories } = useGoalManagementContext()
+  const { error, setError, loading, makeRequest } = useAxios()
+
+  const addCategory = (data) => {
+    setCategories((prev) => [data, ...prev])
+  }
+
+  const sendRequest = (name: string) => {
+    const config = {
+      method: 'post',
+      url: CATEGORIES_URL,
+      params: { name },
+    }
+    makeRequest(config, addCategory)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('got submitted')
+    const form = event.target as HTMLFormElement
+    const formData = new FormData(form)
+    const name = formData.get('name') as string
+    console.log('name: ', name)
+
+    if (name === '') {
+      setError('Must enter a name')
+    } else {
+      setIsAdding(false)
+      sendRequest(name)
+      form.reset()
+    }
+  }
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <section>
+          <label htmlFor='name'>Name</label>
+          <input type='name' name='name' />
+        </section>
+        <button type='submit' className='btn btn-sm'>
+          Add
+        </button>
+      </form>
+    </>
+  )
+}
+export default CategoryForm
