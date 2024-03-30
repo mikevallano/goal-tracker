fe1f526b4c9119596911d696754db35db1c1775d
mike vallano <mikevallano@gmail.com>
2024-03-30 09:02:11 -0400

    15: Add create method in wins controller. Wrap up backend.

--- app/controllers/api/v1/wins_controller.rb
+++ app/controllers/api/v1/wins_controller.rb
@@ -5,4 +5,23 @@ class Api::V1::WinsController < Api::BaseController
     end
     render json: serialized_wins, status: :ok
   end
+
+  def create
+    result = CreateWin.call!(params: create_params)
+    if result.success?
+      render json: result.win
+    else
+      render json: { error: result.error_message }, status: :precondition_failed
+    end
+  end
+
+  private
+
+  def win_params
+    params.permit(:title, :description, :category_id, :happened_at).to_h.symbolize_keys
+  end
+
+  def create_params
+    win_params.merge(user_id: current_user.id)
+  end
 end
--- spec/requests/api/v1/wins_spec.rb
+++ spec/requests/api/v1/wins_spec.rb
@@ -13,4 +13,29 @@ RSpec.describe 'Api::V1::Wins', type: :request do
       expect(response.body).to include(existing_win.title)
     end
   end
+
+  describe 'POST /create' do
+    context 'when successful' do
+      let(:request) do
+        post api_v1_wins_path, params: { title: 'tester', category_id: create(:category).id }
+      end
+
+      it 'is successful' do
+        expect { request }.to change { Win.count }.by(1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when failure' do
+      let(:request) do
+        post api_v1_wins_path, params: { title: '', category_id: create(:category).id }
+      end
+
+      it 'is successful' do
+        expect { request }.not_to(change { Win.count })
+        expect(response).to have_http_status(:precondition_failed)
+        expect(JSON.parse(response.body)['error'].downcase).to include('title')
+      end
+    end
+  end
 end
