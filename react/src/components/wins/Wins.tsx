034eacbd1d554114ab179c2a25f5b0502c00edf8
mike vallano <mikevallano@gmail.com>
2024-03-31 09:13:19 -0400

    15: Add functionality to add wins

--- react/src/components/wins/Wins.tsx
+++ react/src/components/wins/Wins.tsx
@@ -4,12 +4,14 @@ import baseUrl from '../../config'
 import { WinType } from '../../types/WinType'
 import Win from './Win'
 import { requestConfigParams } from '../../hooks/useAxios'
+import AddWinForm from './AddWinForm'
 
 const WINS_URL = `${baseUrl}/api/v1/wins`
 
 const Wins = () => {
-  const { loading, error, makeRequest } = useAxios()
+  const { loading, error, setError, makeRequest } = useAxios()
   const [wins, setWins] = useState<WinType[]>([])
+  const [isAdding, setIsAdding] = useState(false)
 
   const fetchWins = () => {
     const config: requestConfigParams = {
@@ -26,12 +28,26 @@ const Wins = () => {
   return (
     <div>
       <h2>Wins</h2>
+      {isAdding && (
+        <AddWinForm
+          setIsAdding={setIsAdding}
+          setWins={setWins}
+          setError={setError}
+        />
+      )}
+      {!isAdding && (
+        <button className='btn btn-sm' onClick={() => setIsAdding(true)}>
+          Add a win
+        </button>
+      )}
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-      {wins &&
-        wins.map((win) => {
-          return <Win win={win} key={win.id} />
-        })}
+      <div className='cards-container'>
+        {wins &&
+          wins.map((win) => {
+            return <Win win={win} key={win.id} />
+          })}
+      </div>
     </div>
   )
 }
