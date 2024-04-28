ad20055b31633acc5a5700bf7d44f8b1895dfc50
Mike Vallano <mikevallano@gmail.com>
2024-04-28 09:16:46 -0400

    46: fix routing (#58)
    46: Fix routing

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,5 @@
+class PublicController < ApplicationController
+  def index
+    render file: Rails.public_path.join('index.html'), layout: false
+  end
+end
--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ApplicationController
-  def index
-    render file: Rails.public_path.join('public', 'index.html'), layout: false
-  end
-end
